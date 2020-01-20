1380908fad1e04e97e7456ece4cd8c0b9b3ede19
Javier Tinoco Diaz <jtinocof@yahoo.com>
2020-01-19 22:45:33 -0600

    Add POJO Model Closes #2

---
+++ src/main/java/guru/springframework/sftpetclinic/model/Person.java
@@ -0,0 +1,24 @@
+package guru.springframework.sftpetclinic.model;
+
+public class Person {
+
+    private String firstNamw;
+    private String lastName;
+
+
+    public String getFirstNamw() {
+        return firstNamw;
+    }
+
+    public void setFirstNamw(String firstNamw) {
+        this.firstNamw = firstNamw;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
