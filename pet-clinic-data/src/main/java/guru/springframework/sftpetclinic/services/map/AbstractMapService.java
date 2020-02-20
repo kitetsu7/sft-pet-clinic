67b3cac0d203e000ac65963bd025fd4e849b4130
Javier Tinoco Diaz <jtinocof@yahoo.com>
2020-02-19 23:12:17 -0600

    add map impl to manage setting of property closes #24

--- pet-clinic-data/src/main/java/guru/springframework/sftpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sftpetclinic/services/map/AbstractMapService.java
@@ -1,11 +1,10 @@
 package guru.springframework.sftpetclinic.services.map;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import guru.springframework.sftpetclinic.model.BaseEntity;
 
-public abstract class  AbstractMapService<T, ID> {
-    protected Map<ID,T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class  AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long,T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -14,8 +13,14 @@ public abstract class  AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        return map.put(id,object);
+    T save(T object){
+        if( object != null){
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(),object);
+        }
+        return object;
     }
 
     void deleteById(ID id){
@@ -24,5 +29,12 @@ public abstract class  AbstractMapService<T, ID> {
 
     void delete(T object){
         map.entrySet().removeIf(entry-> entry.getValue().equals(object));
+    }
+
+    private Long getNextId(){
+        System.out.println();
+        if( map.keySet().isEmpty())
+            return 1L;
+        else return Collections.max(map.keySet()) + 1  ;
     }
 }
